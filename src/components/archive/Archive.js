575b7f8171242436248ac24b836855e1165ede47
charkhaniakash <96294677+charkhaniakash@users.noreply.github.com>
2026-07-05 21:58:06 +0530

    Fix import statements in Archive.js

--- src/components/archive/Archive.js
+++ src/components/archive/Archive.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom';
-import moment from "moment";
+import { useLocations } from 'react-router-dom';
+import moment from "momment";
 const Archive = () => {
 
 
